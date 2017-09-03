cabd80c4707d86b47a3aa06fa8fb8a9ec9ab3eda
amh <amh@myhost.org>
2017-09-03 20:45:39 +0300

    Scroll controller and viewPort design

--- src/bitmapgrid/ui/ViewPort.java
+++ src/bitmapgrid/ui/ViewPort.java
@@ -2,25 +2,68 @@ package bitmapgrid.ui;
 
 public class ViewPort {
     
-    private int[] portSize;     // Viewport size in screen pixels
-    private float[] origin;     // Image [0, 0] position on projection
-    private float pixelSize;    // Pixel `  size in document units
+    private int[] portSize;
+    private float[] portPosition;
+    private float portDpi;
+    
+    private float[] documentSize;
     
     public void move(int[] offset) {
-        for (int i = 0; i < 2; i++) {
-            origin[i] += offset[i] * pixelSize;
-        }
+        // TODO: wrong way
+      
     }
     
     public void zoom(int[] point, float scale) {
         
     }
     
+    public float getPortDpi() {
+        return portDpi;
+    }
+    
     public int[] getCenter() {
         return new int[] {portSize[0] / 2, portSize[1] / 2};
     }
     
-    public void reset(float[] documentSize, float margin) {
+    public void reset(float[] documentSize) {
         
+    }
+    
+    public int[] getDocumentOverhang() {
+        int[] res = new int[2];
+        for (int i = 0; i < 2 ; i++) {
+            res[i] = (int)Math.max(0, Math.ceil(documentSize[i] - portSize[i] / portDpi)); 
+        }
+        return res;
+    }
+    
+    public int[] getPortOffset() {
+        int[] res = new int[2];
+        for (int i = 0; i < 2; i++) {
+            res[i] = (int)Math.round(portPosition[i] / portDpi);
+        }
+        return res;
+    }
+    
+    public void setPortOffset(int[] offset) {
+        // Clip? throw an exception for testing purposes
+    }
+    
+    public int[] clipPortOffset(int[] offset) {
+        int[] res = new int[2];
+        int[] overhang = getDocumentOverhang();
+        for (int i = 0; i < 2; i++) {
+            res[i] = clipAbs(offset[i], overhang[i] / 2);
+        }
+        return res;
+    }
+    
+    private int clipAbs(int val, int max) {
+        if (val > 0) {
+            return Math.min(val, max);
+        }
+        else {
+            return Math.max(val, -max);
+        }
     }
 }
